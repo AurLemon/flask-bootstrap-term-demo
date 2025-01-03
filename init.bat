30b45e3b1d3a27955db16d243119104f74405222
AurLemon <aurlemoncn@gmail.com>
2025-01-03 13:15:19 +0800

    Update init.bat

--- init.bat
+++ init.bat
@@ -1,36 +1,6 @@
 @echo off
 chcp 65001
 
-:: 检查 Python 装了没
-where python >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Error: Python 3 未安装，请安装后继续操作。
-    exit /b 1
-)
-
-:: 检查 pip 装了没
-python -m pip --version >nul 2>nul
-if %errorlevel% neq 0 (
-    echo Error: pip 未安装，请安装后继续操作。
-    exit /b 1
-)
-
-:: 检查虚拟环境
-if not exist "venv" (
-    echo 虚拟环境未创建，正在创建...
-    python -m venv .venv
-)
-
-call .venv\Scripts\activate.bat
-
-:: 安装依赖
-if exist "requirements.txt" (
-    echo 根据项目依赖描述文件安装依赖包...
-    pip install -r requirements.txt
-) else (
-    echo 依赖描述文件读取有误，请检查项目是否完整。
-)
-
 :: 检查 Node.js 是否安装
 echo 检查 Node.js 是否安装...
 node -v >nul 2>&1
@@ -49,6 +19,28 @@ if %errorlevel% neq 0 (
     start /wait python_installer.exe
 ) else (
     echo Python 已安装.
+)
+
+:: 检查 pip 装了没
+python -m pip --version >nul 2>nul
+if %errorlevel% neq 0 (
+    echo Error: pip 未安装，请安装后继续操作。
+)
+
+:: 检查虚拟环境
+if not exist "venv" (
+    echo 虚拟环境未创建，正在创建...
+    python -m venv .venv
+)
+
+call .venv\Scripts\activate.bat
+
+:: 安装依赖
+if exist "requirements.txt" (
+    echo 根据项目依赖描述文件安装依赖包...
+    pip install -r requirements.txt
+) else (
+    echo 依赖描述文件读取有误，请检查项目是否完整。
 )
 
 :: 激活虚拟环境
