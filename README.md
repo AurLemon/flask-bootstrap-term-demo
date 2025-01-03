a98571b42a92c26cb729015e9a2d93e1b98865f7
AurLemon <aurlemoncn@gmail.com>
2025-01-03 13:08:52 +0800

    Daily commit

--- README.md
+++ README.md
@@ -18,9 +18,9 @@
 
 0. **克隆仓库和安装运行环境。** 使用 `git clone https://github.com/AurLemon/flask-bootstrap-term-demo.git` 从 GitHub 克隆仓库。确保安装好 Python 3 和 pip。
 
-1. **配置依赖包和虚拟环境。** 项目提供好了初始化脚本，在 Linux 下，切换到项目目录下以后，执行 `chmod +x init.sh` 赋予脚本执行权限后，继续执行 `./init.sh` 开始初始化；若在 Windows 下，双击打开 init.bat。
+1. **配置依赖包和虚拟环境。** 项目提供好了初始化脚本，在 Windows 下，双击打开 init.bat，配置好环境。
 
-2. **安装成功后，根据系统环境激活虚拟环境** 。Linux 使用 `source .venv/bin/activate`；Windows 使用 `call .venv\Scripts\activate.bat`。激活成功后终端主机名前会出现 `(.venv)` 前缀。
+2. **安装成功后，根据系统环境激活虚拟环境** 。使用 `call .venv\Scripts\activate.bat`。激活成功后终端主机名前会出现 `(.venv)` 前缀。
 
 3. **在虚拟环境下启动服务。** 使用 `python app.py` 启动服务。出现以下信息代表成功：
 ```shell
@@ -40,7 +40,4 @@ Press CTRL+C to quit
 - [x] 前端界面
 - [x] 前端渲染
 - [x] 后端路由
-    - [x] Blog
-    - [x] Login
-    - [x] Admin
 - [x] 后端模拟数据
--- init.bat
+++ init.bat
@@ -1,6 +1,5 @@
-chcp 65001
-
 @echo off
+chcp 65001
 
 :: 检查 Python 装了没
 where python >nul 2>nul
@@ -32,14 +31,35 @@ if exist "requirements.txt" (
     echo 依赖描述文件读取有误，请检查项目是否完整。
 )
 
+:: 检查 Node.js 是否安装
+echo 检查 Node.js 是否安装...
+node -v >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Node.js 没有安装，正在安装 Node.js...
+    start /wait nodejs_installer.msi
+) else (
+    echo Node.js 已安装.
+)
+
+:: 检查 Python 是否安装
+echo 检查 Python 是否安装...
+python --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Python 没有安装，正在安装 Python...
+    start /wait python_installer.exe
+) else (
+    echo Python 已安装.
+)
+
 :: 激活虚拟环境
 echo ==========
 echo.
 echo 输入 call .venv\Scripts\activate.bat 来激活虚拟环境；deactivate 退出虚拟环境。
+echo 随后输入 python app.py 即可启动服务器。
 echo.
 echo ==========
 
 echo.
 echo 初始化完成 √
 
-pause
+set /p userInput=call .venv\Scripts\activate.bat
